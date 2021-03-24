3d2fb607437c0dbe4f2821c7a44074b8f2f75488
quinlanharsch <quinlanharsch@gmail.com>
2021-03-24 16:42:27 -0400

    gh-pages

---
+++ precache-manifest.4a1bfed29566a3e957939dcc548df556.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c645b2aaa1e397832e5fd005a0ffcb24",
+    "url": "/free-warehouse/browserconfig.xml"
+  },
+  {
+    "revision": "377f0ccc67bb57091090",
+    "url": "/free-warehouse/css/app.5d25f328.css"
+  },
+  {
+    "revision": "5364455c67d0c18e4cca2c81f34fc962",
+    "url": "/free-warehouse/index.html"
+  },
+  {
+    "revision": "377f0ccc67bb57091090",
+    "url": "/free-warehouse/js/app.a05ab9aa.js"
+  },
+  {
+    "revision": "0dddd2ab55dc073dd3ce",
+    "url": "/free-warehouse/js/chunk-vendors.cd6fce00.js"
+  },
+  {
+    "revision": "de2cfd5c05773f5e86c04c904aebf783",
+    "url": "/free-warehouse/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/free-warehouse/robots.txt"
+  },
+  {
+    "revision": "68bcc5adcca6d058942de5a792f761f6",
+    "url": "/free-warehouse/site.webmanifest"
+  }
+]);
